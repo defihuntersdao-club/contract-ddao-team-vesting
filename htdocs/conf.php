a08e27e1ddd9b8a6dc2e7f3b7703911ebff2380c
Liksagen.com <github-lxgn@ex.liksagen.com>
2022-03-24 20:27:02 +0200

    Add Enable options

--- htdocs/conf.php
+++ htdocs/conf.php
@@ -16,4 +16,5 @@ $chain_id = 137;
 
 $contractAddress = "0x2935890863a2235bfd8876ff8e92b2785a9a20a7";
 $contractAddress = "0x2a968bc126E6afFAF27f88cd11b06a6E548135d1";
+$contractAddress = "0x724E6297c51567043Ff3886093dCA5F540a857D2";
 ?>
